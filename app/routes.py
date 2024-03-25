e7d3ecb7171948fc1dcbb6e470efcabd5cac3679
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-03-25 20:57:05 +0000

    templates set

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+    user = {'username':'Vijay-Vaddi'}
+    return '''
+<h1> Hi, '''+user['username']+'''
+'''
