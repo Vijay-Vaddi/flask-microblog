e12d662b01548509e810badbb22785c44e932e72
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-03-25 21:35:45 +0000

    base extend

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,15 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username':'Vijay-Vaddi'}
-    return render_template("index.html", user=user)
+    posts =[ 
+        {
+            'author':{'username':'Vijay-Vaddi'},
+            'body':"This is the new beginning"
+        },
+        {
+            'author':{'username':'Obi-Wan Kenobi'},
+            'body':"I have failed you Anakin"
+        },
+    ]
+    return render_template("index.html", title='Home', user=user, posts=posts)
+
