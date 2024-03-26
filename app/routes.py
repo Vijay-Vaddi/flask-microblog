77c00a270e95130ad0979f316722fc2fc85cfb6c
Vijay-Vaddi <59910439+Vijay-Vaddi@users.noreply.github.com>
2024-03-26 11:35:41 +0000

    form view

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-
+from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
@@ -16,4 +16,12 @@ def index():
         },
     ]
     return render_template("index.html", title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash(f"Login requested for user {form.username}")
+    return render_template('/login.html', title='Log In', form=form)
 
