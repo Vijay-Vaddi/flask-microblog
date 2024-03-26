cd1d02cee8c694c04a1b8ba2317cc2226f947d17
Vijay-Vaddi <59910439+Vijay-Vaddi@users.noreply.github.com>
2024-03-26 10:32:19 +0000

    forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
-# imported after creating app to avoid common flask problem of secular dependencies 
+app.config.from_object(Config)
 
+''' imported after creating app to avoid common 
+flask problem of secular dependencies''' 
 from app import routes 
