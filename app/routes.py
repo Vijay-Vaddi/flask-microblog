c40aeb4b3742bed262203ebe9a264a94fd974111
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-04-09 12:09:05 +0100

    login route

--- app/routes.py
+++ app/routes.py
@@ -50,11 +50,13 @@ def logout():
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+    # redirect if logged in
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     
     form = RegistrationForm()
 
+    # 
     if form.validate_on_submit():
         user = User(username=form.username.data,
                     email=form.email.data)
