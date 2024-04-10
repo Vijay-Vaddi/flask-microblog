43374d6bca6d5e4fb303453bf01339a1ca263e6d
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-04-10 21:00:04 +0100

    custom error pages

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -14,4 +13,5 @@ login.login_view = 'login'
 
 ''' imported after creating app to avoid common 
 flask problem of secular dependencies''' 
-from app import routes, models 
+from app import routes, models, errors
+ 
