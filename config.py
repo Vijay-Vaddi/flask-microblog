a8af9958bcbb7b6aebc468b1cdeea853e887d802
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-04-26 20:34:32 +0100

    .env, configs

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os 
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
@@ -10,8 +12,8 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com' or os.environ.get('MAIL_SERVER')
     MAIL_PORT = 587 or int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = 1 or os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = 'vijayzvaddi@gmail.com' or os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = '12342143423442' or os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['vijayzvaddi@gmail.com']
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     POSTS_PER_PAGE = 3
