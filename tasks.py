7699c9d5114255d26238ae6a159cdce40504f05a
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-05-19 18:11:35 +0100

    tasks export/send mail

--- tasks.py
+++ tasks.py
@@ -1,7 +1,10 @@
 from app import create_app
 from app import db
-from app.models import Task
+from app.models import Task, User, Post
 from rq import get_current_job
+import time, json, sys
+from app.email import send_email
+from flask import render_template
 
 app = create_app()
 app.app_context().push()
@@ -19,3 +22,31 @@ def _set_task_progess(progress):
             task.complete = True
         
         db.session.commit()
+
+def export(user_id):
+    try:
+        # 
+        # 
+        _set_task_progess(0)
+        user = User.query.get(user_id)
+        i = 0
+        data = []
+        total_posts = user.posts.count()
+        for post in user.posts.order_by(Post.timestamp.asc()):
+            data.append({'body':post.body, 'timestamp':post.timestamp.isoformat()+'z'})
+            time.sleep(5)
+            i=i+1
+            _set_task_progess(100*i//total_posts)
+        
+        send_email('Your blog posts', 
+                   sender=app.config['ADMINS'][0], recipients=[user.email],
+                   text_body=render_template("email/export_posts.txt", user=user), 
+                   html_body=render_template('email/export_posts.html', user=user),
+                   attachments=[('posts.json', 'application/json', json.dumps({'posts':data}, indent=4))], sync=True)                    
+    except:
+        _set_task_progess(100)
+        app.logger.error('Unhandled exception', exc_info=sys.exec_info())
+        
+    finally:
+        _set_task_progess(100)
+        
