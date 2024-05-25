62bc4ac17e79b4fb5e28205f0f992a2c2243e235
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-05-25 12:21:02 +0100

    http basic auth for api

---
+++ app/api/auth.py
@@ -0,0 +1,19 @@
+from flask_httpauth import HTTPBasicAuth
+from app import db
+from app.models import User
+from app.api.errors import error_response
+
+basic_auth = HTTPBasicAuth()
+
+# function to check credentials and return user
+@basic_auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        return user
+
+# function to handle 401 unauthorized error
+@basic_auth.error_handler
+def basic_auth_error(status):
+    return error_response(status)
+
